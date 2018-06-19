1a8ccbca7235ce144cd910b41294b732b7ce6806
Ben Howes <ben@ben-howes.co.uk>
2018-06-19 14:43:27 +0100

    added description to NN

--- solver/algs/nearest.py
+++ solver/algs/nearest.py
@@ -6,13 +6,21 @@ from .base import RouteAlgorithm
 logger = logging.getLogger(__name__)
 
 class NearestRouteAlg(RouteAlgorithm):
-    """Creates a route by visiting each nearest neighbour in turn"""
+    """Creates a route by visiting each nearest neighbor in turn"""
 
     cli_name = "nn"
 
     def get_route(self, route: Route) -> Route:
         """
-    
+        This works by coping Points over from the input route to the output
+        route ordered by hopping to the next nearest Point over each iteration
+        until all nodes have been visited.
+
+        Args:
+            route (Route) - The starting route
+
+        Returns:
+            Route - The result of repeatedly hopping to the nearest neighbor
         """
         nn_route = Route()
 
@@ -24,7 +32,7 @@ class NearestRouteAlg(RouteAlgorithm):
 
             # 1. create a list of tuples with the point and the distance
             # 2. pick the closest
-            # 3. remove that from the source route and add to the new route 
+            # 3. remove that from the source route and add to the new route
             distances = [(other, current_point.distance(other)) for other in route]
             next_point, distance = min(distances, key=lambda x: x[1])
             route.remove(next_point)
